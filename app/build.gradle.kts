914e544119bf8084a90d03572a56d0136635f4b1
danil <danilovmv06@gmail.com>
2026-02-04 00:50:13 +0100

    init 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,35 @@
 plugins {
     id("java")
+    id("groovy")
 }
 
 group = "algotrader"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "groovy")
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+
+        testImplementation("org.apache.groovy:groovy:4.0.15")
+
+        testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
+
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
 }
 
 tasks.test {
